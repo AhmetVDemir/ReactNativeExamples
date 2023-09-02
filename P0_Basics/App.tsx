80d03bfc432428e2f5f0976507b7f2ee771f0e9c
Ahmet Demir <mumaru171@gmail.com>
2023-09-02 22:35:29 +0300

    p1 added

---
+++ P0_Basics/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native'
+
+
+
+function App(){
+
+return(
+
+
+  <SafeAreaView>
+    <View>
+      <Text> Hi Test </Text>
+    </View>
+  </SafeAreaView>
+)
+
+
+}
+
+export default App;
